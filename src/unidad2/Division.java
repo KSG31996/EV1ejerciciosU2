00dafbcccd5660ccd31f33ac802477cadbdf3e40
KSG31996 <ksg31996@educastur.es>
2020-12-05 00:42:35 +0100

    Ejercicio 1 - Unidad 2

---
+++ src/unidad2/Division.java
@@ -0,0 +1,10 @@
+package unidad2;
+
+public class Division {
+
+	public static void main(String[] args) {
+		//Ejercicio 1 - Unidad 2
+		System.out.printf("1234 / 531 = %15.2f",  1234d / 532d);
+	}
+
+}
